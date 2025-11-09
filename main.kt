0df1d6a1734e270de626b95ac57a68e79aebc77e
Heo KyuYoung <0xq0h3@gmail.com>
2025-11-09 13:45:25 +0900

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,9 +1,16 @@
 enum class Color {
   Red,
   Green;
+
+  func getColorName(): String {
+    return when(this) {
+      Color.Red -> "This is Red"
+      Color.Green -> "This is Green"
+    }
 }
 
 fun main() {
   val color = Color.Red
   println("Hello, World!")
+  println(color.getColorName())
 }

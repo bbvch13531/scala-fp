f5796f2a559b538c57153d2990143242aa55c5fe
Heo KyuYoung <0xq0h3@gmail.com>
2025-11-09 13:39:45 +0900

    Add getColorName function to Color enum

--- main.kt
+++ main.kt
@@ -1,9 +1,16 @@
 enum class Color {
   Red,
   Green;
+
+  fun getColorName(): String {
+    return when(this) {
+      Color.Red -> "This is Red"
+      Color.Green -> "This is Green"
+    }
 }
 
 fun main() {
   val color = Color.Red
   println("Hello, World!")
+  println(color.getColorName())
 }
